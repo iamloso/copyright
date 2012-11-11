23261d795c2032ea4993a63f161a0a2a59ed2eeb
loso <iamloso@qq.com>
2012-11-11 22:40:42 +0800

    jjj

--- Lib/Action/Home/IndexAction.class.php
+++ Lib/Action/Home/IndexAction.class.php
@@ -11,6 +11,65 @@ class IndexAction extends HomeAction{
 		$this->assign('title',C('site_name').''.C('site_by'));
 		$this->assign('pplist',A("Home"));
 		$this->assign('mhot_data', $mhot_data);
+		$menu_data = F('_ppvod/listtree');
+		foreach($menu_data as $key => &$value)
+		{
+			 $un_array = array(21, 22);
+			 $temp_array = array();
+	         if(in_array($value['list_id'], $un_array))
+			 {
+			     continue; 
+			 }	
+			 else
+			 {
+			     foreach($value['son'] as $k => $v)
+				 {
+				    $temp_array[] = $v['list_id']; 
+				 }
+				 $list_ids = implode(',', $temp_array);
+                 $sql = " select * from pp_vod where vod_hot=1 and vod_cid in ('{$list_ids}') and vod_del=0";
+				 $vod_data = M()->query($sql);
+			     $value['son'] = $vod_data; 
+			 }
+		}
+		$this->assign('hot_data', $menu_data);
 	    $this->display('piao_index');
     }	
+
+	public function ajax_menu()
+	{
+		 $menu_data = F('_ppvod/listtree');
+		 foreach($menu_data as $key => $value)
+		 {
+			 if($_POST['is_two']==1)
+			 {	 
+				 foreach($value['son'] as $k => $v)
+				 {
+					 if($v['list_id']==$_POST['list_id'])
+					 {
+					     $three_menu = $v['son']; 
+		                 exit(json_encode($three_menu));
+					 }
+					 else
+					 {
+					     continue;
+					 }
+				 }
+				
+			 } 	 
+			 else
+			 {
+			     if($value['list_id']==$_POST['list_id']) 
+				 {
+					 $two_menu = $value['son'];		 
+					 exit(json_encode($two_menu));
+				 }
+				 else
+				 {
+					 continue;
+				 }		 
+			 
+			 }
+		 }	
+	}
 }?>
