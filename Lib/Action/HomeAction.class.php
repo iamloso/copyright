5c5003112386778b94302fbdd3588ecf74f93712
loso <iamloso@126.com>
2012-11-13 18:55:35 +0800

    结婚

--- Lib/Action/HomeAction.class.php
+++ Lib/Action/HomeAction.class.php
@@ -15,12 +15,33 @@ class HomeAction extends AllAction{
 		$style['copyright']=C('site_copyright');
 		$style['tongji']=C('site_tongji');
 		$style['icp']=C('site_icp');
-		$style['model']=strtolower(MODULE_NAME);
+		$style['model'] =strtolower(MODULE_NAME);
 		$style['action']=strtolower(ACTION_NAME);
 		C('TOKEN_NAME','__ppvod__');	
         $this->assign($style);
 		$this->assign('pplink',F('_ppvod/link'));
-		$this->assign('ppmenu',F('_ppvod/listtree'));
+		$menu_data = F('_ppvod/listtree');
+		foreach($menu_data as $key => &$value)
+		{
+			 $un_array = array(21, 22);
+			 $temp_array = array();
+	         if(in_array($value['list_id'], $un_array))
+			 {
+			     continue; 
+			 }	
+			 else
+			 {
+			     foreach($value['son'] as $k => $v)
+				 {
+				    $temp_array[] = $v['list_id']; 
+				 }
+				 $list_ids = implode(',', $temp_array);
+                 $sql = " select count(*) as count from pp_vod where vod_hot=1 and vod_cid in ('{$list_ids}') and vod_del=0";
+				 $vod_data = M()->query($sql);
+			     $value['count'] = $vod_data[0]['count']; 
+			 }
+		}
+		$this->assign('ppmenu', $menu_data);
     }	
 	public function showmenu(){
 	    return F('_ppvod/listtree');
